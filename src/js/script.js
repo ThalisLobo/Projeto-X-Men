74867c6fdce406a23234739d96059d459b666059
Thalis <thalislobodev@gmail.com>
2023-07-08 22:53:33 -0300

    First Commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,26 @@
-const personagens = document.querySelectorAll(".personagem")
+const personagens = document.querySelectorAll(".personagem");
 
 personagens.forEach((personagem) => {
 personagem.addEventListener("mouseenter", () => {
 
         if(window.innerWidth < 450) {
-            window.scrollTo({top: 0, behavior: "smooth"})
+            window.scrollTo({top: 0, behavior: "smooth"});
         }
         
-        const personagemSelecionado = document.querySelector(".selecionado")
-        personagemSelecionado.classList.remove("selecionado")
-        personagem.classList.add("selecionado")
+        const personagemSelecionado = document.querySelector(".selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+        personagem.classList.add("selecionado");
 
-        const imagemPersonagemGrande = document.querySelector(".personagem-grande")
+        const imagemPersonagemGrande = document.querySelector(".personagem-grande");
 
         const idPersonagem = personagem.attributes.id.value
-        imagemPersonagemGrande.src= `./src/imagens/card-${idPersonagem}.png`
+        imagemPersonagemGrande.src= `./src/imagens/card-${idPersonagem}.png`;
 
-        const nomePersonagem = document.getElementById("nome-personagem")
-        nomePersonagem.innerText = personagem.getAttribute("data-name")
+        const nomePersonagem = document.getElementById("nome-personagem");
+        nomePersonagem.innerText = personagem.getAttribute("data-name");
 
-        const descriçãoPersonagem = document.getElementById("descricao-personagem")
-        descriçãoPersonagem.innerText = personagem.getAttribute("data-description")
+        const descriçãoPersonagem = document.getElementById("descricao-personagem");
+        descriçãoPersonagem.innerText = personagem.getAttribute("data-description");
 
     })
 })
